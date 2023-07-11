75a4a5c236d7582f01a6938015d92c4bd4cce16c
Tom Eldridge <tom.eldridge@nhs.net>
2023-07-11 08:13:50 +0100

    futher edits

--- Python Projects/web_scrapers/interface_webscraper.py
+++ Python Projects/web_scrapers/interface_webscraper.py
@@ -10,7 +10,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
 from selenium import webdriver
 import time
 import re
