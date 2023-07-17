032d0050d6a43b3fe75f353d09f437e205d72c1d
Tom Eldridge <tom.eldridge@nhs.net>
2023-07-17 08:32:34 +0100

    added phone number and office scrape

--- python_projects/web_scrapers/kirkland_ellis.py
+++ python_projects/web_scrapers/kirkland_ellis.py
@@ -34,19 +34,38 @@ for url in urls:
         last_height = new_height
 
     data = []
-    divs = driver.find_elements(By.CSS_SELECTOR, "div.person-result__main")
+    divs = driver.find_elements(By.CSS_SELECTOR, "div.person-result.person-result_transition-in")
     
     for div in divs:
         name = div.find_element(By.CSS_SELECTOR, "h3.person-result__name").text
         level = div.find_element(By.CSS_SELECTOR, "div.person-result__level").text
         email = div.find_element(By.CSS_SELECTOR, "a.person-result__email").text
-        data.append({'Name': name, 'Level': level, 'Email': email})
+        offices = div.find_elements(By.CSS_SELECTOR, "div.person-result__office")
+        if offices:
+            if len(offices) > 1:
+                office1 = offices[0].find_element(By.CSS_SELECTOR, "h4.person-result__office-title").text
+                phone1 = offices[0].find_element(By.CSS_SELECTOR, "a.person-result__title").text
+                office2 = offices[1].find_element(By.CSS_SELECTOR, "h4.person-result__office-title").text
+                phone2 = offices[1].find_element(By.CSS_SELECTOR, "a.person-result__title").text
+            else:
+                office1 = offices[0].find_element(By.CSS_SELECTOR, "h4.person-result__office-title").text
+                phone1 = offices[0].find_element(By.CSS_SELECTOR, "a.person-result__title").text
+                office2 = None
+                phone2 = None
+        else:
+            office1 = None
+            phone1 = None
+            office2 = None
+            phone2 = None
 
+        data.append({'Name': name, 'Level': level, 'Email': email, 'Office1': office1,
+                      'Phone1': phone1, 'Office2': office2, 'Phone2': phone2})
 
     df_new = pd.DataFrame(data)
     df = pd.concat([df, df_new], ignore_index=True)
     
 driver.quit()
 print(df)
+df.to_csv('/home/tome/portfolio/test_data/candidates_output.csv')
 
 
